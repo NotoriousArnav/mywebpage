f52a35d3ed32ef641c18a87f4ca35d245bd000c3
Arnav ghosh <arnavghosh.official.2004@gmail.com>
2022-09-26 18:54:16 +0530

    v2 Final Version

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, jsonify, render_template, request
 import json
 import requests
 
 app = Flask(__name__, template_folder='templates', static_url_path='/static/', static_folder='static')
-
+"""
 def get_client_location(ip):
     resp = requests.get(f'https://ipinfo.io/{ip}/json')
     dt = resp.json()
@@ -12,7 +12,7 @@ def get_client_location(ip):
     else:
         addr=f"{dt['city']},{dt['region']},{dt['country']} or {dt['loc']}"
     return addr
-
+"""
 @app.after_request
 def add_header(r):
     r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
@@ -24,5 +24,28 @@ def add_header(r):
 @app.route('/')
 def index():
     with open('links.json','r') as f: links=json.load(f)
-    content={'title':'Welcome to Just Arnav', 'links':links, 'client_loc':get_client_location(request.environ['REMOTE_ADDR'])}
+    content={'title':'Welcome to Just Arnav', 'links':links}# 'client_loc':get_client_location(request.environ['REMOTE_ADDR'])}
     return render_template('index.html', content=content)
+
+@app.route('/json')
+def index_json():
+    with open('links.json','r') as f: links=json.load(f)
+    resp = {
+                'status':['Working',True, 200],
+                'message':{
+                        'text':"""
+Hi, I am Arnav Ghosh and This is my Website and you are welcome.
+Caution use JQ if you are in a Terminal
+                        """,
+                        'links':links
+                    }
+            }
+    return jsonify(resp)
+
+@app.route('/terminal')
+def index_terminal():
+    with open('term.txt', 'rt') as f: termtext = f.read()
+    return termtext
+
+if __name__=='__main__':
+    app.run()
